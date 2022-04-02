5c8cc34766f44504cbf2f8ed15f828f321cf9b8a
jeffreylouie <louiej715@strose.edu>
2022-04-01 20:17:45 -0400

    Player Template Movement + Camera Follow
    -White square replaced by player sprites (temporary until final sprites are done)
    -Player now has animations, albeit primitive ones due to lack of sprites
    -The camera now follows the player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = target.position + offset;
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -16,10 +17,15 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()      //movement
     {
-        rb.MovePosition(rb.position + movement * moveSpeed);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
