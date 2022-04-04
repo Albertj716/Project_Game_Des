f19dfd749707b047340e02611fac5f4e4cd61dd7
kpbennett12 <kpbennett12@hotmail.com>
2022-04-04 13:49:05 -0400

    Volume slider now functions, no music yet though

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+}
