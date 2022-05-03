a70ed0e0dad2239d4e88f5e3e46d22f234fca00c
kpbennett12 <kpbennett12@hotmail.com>
2022-05-03 10:00:26 -0400

    Changed Quit to About page

---
+++ Assets/Menus/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void Play()
+    {
+        PauseMenu.GameIsPaused = false;
+        SceneManager.LoadScene("Gameplay");
+    }
+}
